f10ce6fcd35e4dda5c09f1676b0959986ee8ded7
Viktor <viktor.sidiak@gmail.com>
2020-03-17 04:02:22 +0200

    commit

---
+++ src/test/java/com/startingSpringFramework/Spring5WebApp/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startingSpringFramework.Spring5WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
